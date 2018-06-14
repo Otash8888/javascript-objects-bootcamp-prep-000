9fdac62cd49556c81b9fd8cc8c9637515273a34b
Otabek Kamalov <otakam86@gmail.com>
2018-06-14 22:17:23 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {
+  key:value
+}
+function updatePlaylist(object,artName,song){
+  //object[artName]=song
+  return Object.assign(object,{[artName]:song})
+}
+function removeFromPlaylist(object,artName){
+  
+  return delete object[artName];
+}
